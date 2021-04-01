3a8aed113ba0ad765efa5445c06b8af8e5ce2837
marinchr21 <59925466+marinchr21@users.noreply.github.com>
2021-04-01 09:08:51 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,25 +8,34 @@ int petChoice = 0;
 
   Aquarium fishtank = new Aquarium();
   Dog dog = new Dog();
+  Cat cat = new Cat();
+  animalYears animalyears = new animalYears();
 
 
 
   System.out.println("Please select a pet");
   System.out.println("Dog = 1");
   System.out.println("Fish = 2");
+  System.out.println("Cat = 3");
 
 petChoice = scan.nextInt();
 
 if (petChoice == 1) {
   dog.createDog();
+  animalyears.createanimalYears1();
 }
 else if (petChoice == 2) {
   fishtank.createAquarium();
+  animalyears.createanimalYears2();
+}
+else if (petChoice == 3) {
+  cat.createCat();
 }
 else{
   System.out.print("Enter a Valid Choice");
   petChoice = scan.nextInt();
 }
+ 
   
   }
 }

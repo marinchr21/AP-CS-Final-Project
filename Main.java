c938222165e702c9b9a6fcd61cf931e8f1944414
marinchr21 <59925466+marinchr21@users.noreply.github.com>
2021-05-12 10:30:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,20 @@
+//Christopher Maring
+//Animal Planner Project
+/*
+Some Java Swing Referenced from:
+https://www.javatpoint.com/java-jtable
+*/
+
+
+import javax.swing.*;
+import java.awt.event.*;
 import java.util.*;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
 
-Scanner scan = new Scanner(System.in);
+Home home = new Home();
 
 
-  Aquarium fishtank = new Aquarium();
-  Dog dog = new Dog();
-  Cat cat = new Cat();
-
-
-  int petChoice = 0;
-  System.out.println("Please select a pet");
-  System.out.println("Dog = 1");
-  System.out.println("Fish = 2");
-  System.out.println("Cat = 3");
-
-petChoice = scan.nextInt();
-
-while (petChoice > 3) {
-  System.out.print("Enter a Valid Choice: ");
-  petChoice = scan.nextInt();
-  }
-
-if (petChoice == 1) {
-  dog.createDog();
-  dog.dogYears();
-}
-else if(petChoice == 2) {
-  fishtank.createAquarium();
-  
-}
-else if (petChoice == 3) {
-  cat.createCat();
-  cat.catYears();
-}
-
-  
-
- 
-  
   }
 }
